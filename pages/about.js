51b54f49d2cd3b5025323a036133e7cf807fd292
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2023-02-11 03:33:36 +0500

    next js learning

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <>
+    <h1 style={{color:"orange"}}>
+      About us
+    </h1>
+    </>
+  )
+}
+
+export default about
---
+++ pages/product/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../../styles/Product.module.css';
+
+const index = () => {
+  return (
+    <div className={styles.product}>
+      <h1>
+      product
+      </h1>
+    </div>
+  )
+}
+
+export default index
